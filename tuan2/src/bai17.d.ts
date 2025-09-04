6af3f2efac791e6780cafde35f034ce183f8cc7d
toankii1 <toannguyenkim046@gmail.com>
2025-09-04 23:43:20 +0700

    bai17

---
+++ tuan2/src/bai17.d.ts
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=bai17.d.ts.map
---
+++ tuan2/src/bai17.ts
@@ -0,0 +1,17 @@
+async function doubleAfter1s(num: number): Promise<number> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(num * 2);
+    }, 1000);
+  });
+}
+
+async function run() {
+  const promises = [1, 2, 3, 4].map(n => doubleAfter1s(n));
+
+  for await (const result of promises) {
+    console.log("Result:", result);
+  }
+}
+
+run();
